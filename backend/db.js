b243f063cd0f83860cd88580e2898d27a8a4a605
Sankalp <neel007.sk@gmail.com>
2022-01-30 13:34:35 +0530

    commit 1

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const mongooseURI = "mongodb+srv://Sankalp:sankalp123@cluster0.i1h4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
+const connectToMongo = () =>{
+    mongoose.connect(mongooseURI, ()=>{
+        console.log("Connection Success")
+    })
+}
+
+
+module.exports = connectToMongo;
---
+++ backend/index.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+
+
+const connectToMongo = require('./db');
+connectToMongo();
+
+
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
